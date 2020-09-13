565ba2e945e422d083507fb6cfe1df63566c36ad
Manoel Mecias <mmnc12@gmail.com>
2020-09-13 11:03:04 -0300

    Criando o PageAdmin e tplAdmin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,15 @@
+<?php 
+
+	namespace Hcode;
+
+	class PageAdmin extends Page {
+
+		public function __construct($otps = array(), $tpl_dir = "/views/admin/")
+		{
+
+			parent::__construct($otps, $tpl_dir);
+
+		}
+	}
+
+ ?>
