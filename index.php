fa1ec12febb63d3833525fdca22ba76db6e6b341
Manoel Mecias <mmnc12@gmail.com>
2020-09-20 16:21:47 -0300

    CRUD do usuario

--- index.php
+++ index.php
@@ -86,15 +86,30 @@ $app->get("/admin/users/:iduser/delete", function($iduser) {
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
 });
 
 $app->get("/admin/users/:iduser", function($iduser){
-
-	User::verifyLogin();
-
-	$page = new PageAdmin();
-
-	$page->setTpl("users-update");
+ 
+   User::verifyLogin();
+ 
+   $user = new User();
+ 
+   $user->get((int)$iduser);
+ 
+   $page = new PageAdmin();
+ 
+   $page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
 });
 
 $app->post("/admin/users/create", function() {
@@ -117,6 +132,19 @@ $app->post("/admin/users/create", function() {
 $app->post("/admin/users/:iduser", function($iduser) {
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
 
 });
 
