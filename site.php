7b9d93a531d466589240c75ebad3eadeb095f193
Manoel Mecias <mmnc12@gmail.com>
2020-10-11 22:05:32 -0300

    Carrinho Calculo de frete

--- site.php
+++ site.php
@@ -65,10 +65,11 @@ $app->get("/cart", function(){
 	$cart = Cart::getFromSession();
 
 	$page = new Page();
-
+	
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 
 });
@@ -121,6 +122,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
 });
 
 ?>
